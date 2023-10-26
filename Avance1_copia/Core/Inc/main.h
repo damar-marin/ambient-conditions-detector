598ec99211ffa2459a6a8665118f454efe67a9cb
damar-marin <138634163+damar-marin@users.noreply.github.com>
2023-10-25 20:02:14 -0600

    added projects to repo

---
+++ Avance1_copia/Core/Inc/main.h
@@ -0,0 +1,97 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f1xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define LUZ_Pin GPIO_PIN_0
+#define LUZ_GPIO_Port GPIOA
+#define BUZZER_Pin GPIO_PIN_10
+#define BUZZER_GPIO_Port GPIOB
+#define LED_2R_Pin GPIO_PIN_11
+#define LED_2R_GPIO_Port GPIOB
+#define LED_1R_Pin GPIO_PIN_12
+#define LED_1R_GPIO_Port GPIOB
+#define LED_2A_Pin GPIO_PIN_13
+#define LED_2A_GPIO_Port GPIOB
+#define LED_1A_Pin GPIO_PIN_14
+#define LED_1A_GPIO_Port GPIOB
+#define LED_2V_Pin GPIO_PIN_15
+#define LED_2V_GPIO_Port GPIOB
+#define LED_1V_Pin GPIO_PIN_8
+#define LED_1V_GPIO_Port GPIOA
+#define BTN_LUZ_Pin GPIO_PIN_9
+#define BTN_LUZ_GPIO_Port GPIOA
+#define BTN_LUZ_EXTI_IRQn EXTI9_5_IRQn
+#define BTN_HUM_Pin GPIO_PIN_10
+#define BTN_HUM_GPIO_Port GPIOA
+#define BTN_HUM_EXTI_IRQn EXTI15_10_IRQn
+#define BTN_TEMP_Pin GPIO_PIN_11
+#define BTN_TEMP_GPIO_Port GPIOA
+#define BTN_TEMP_EXTI_IRQn EXTI15_10_IRQn
+#define BTN_ONOFF_Pin GPIO_PIN_15
+#define BTN_ONOFF_GPIO_Port GPIOA
+#define BTN_ONOFF_EXTI_IRQn EXTI15_10_IRQn
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
